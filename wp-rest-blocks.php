eb59f8fa1a4b1e5d40e38255f0983ef2b1f29718
Jonathan Harris <jon@spacedmonkey.co.uk>
2019-06-25 16:54:12 +0100

    Init

---
+++ wp-rest-blocks.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     REST API blocks
+ * Plugin URI:      https://github.com/spacedmonkey/wp-rest-blocks
+ * Description:     Add gutenberg blocks into the api
+ * Author:          Jonathan Harris
+ * Author URI:      https://www.spacedmonkey.com
+ * Text Domain:     wp-rest-blocks
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Rest_Blocks
+ */
+
+// Your code starts here.
