b5b0e50cf736bc55a4e0395febfcd0430302ad0b
Jonathan Harris <jon@spacedmonkey.co.uk>
2019-06-27 17:34:14 +0100

    Fix lints

--- tests/test-blocks.php
+++ tests/test-blocks.php
@@ -47,15 +47,15 @@ class BlocksTest extends WP_UnitTestCase {
 		);
 
 		$mixed_post_content = 'before' .
-		                      '<!-- wp:core/fake --><!-- /wp:core/fake -->' .
-		                      '<!-- wp:core/fake_atts {"value":"b1"} --><!-- /wp:core/fake_atts -->' .
-		                      '<!-- wp:core/fake-child -->
+							  '<!-- wp:core/fake --><!-- /wp:core/fake -->' .
+							  '<!-- wp:core/fake_atts {"value":"b1"} --><!-- /wp:core/fake_atts -->' .
+							  '<!-- wp:core/fake-child -->
 			<p>testing the test</p>
 			<!-- /wp:core/fake-child -->' .
-		                      'between' .
-		                      '<!-- wp:core/self-close-fake /-->' .
-		                      '<!-- wp:custom/fake {"value":"b2"} /-->' .
-		                      'after';
+							  'between' .
+							  '<!-- wp:core/self-close-fake /-->' .
+							  '<!-- wp:custom/fake {"value":"b2"} /-->' .
+							  'after';
 
 		self::$post_with_blocks_id = $factory->post->create(
 			array(
@@ -109,8 +109,8 @@ class BlocksTest extends WP_UnitTestCase {
 		$data = Data\blocks_get_callback( $object );
 		$this->assertTrue( is_array( $data ) );
 		$this->assertEquals( 5, count( $data ) );
-		$this->assertEquals( 'core/fake', $data[0]["blockName"] );
-		$this->assertEquals( 'core/fake_atts', $data[1]["blockName"] );
+		$this->assertEquals( 'core/fake', $data[0]['blockName'] );
+		$this->assertEquals( 'core/fake_atts', $data[1]['blockName'] );
 	}
 
 	/**
@@ -120,9 +120,9 @@ class BlocksTest extends WP_UnitTestCase {
 		$object = [ 'content' => [ 'raw' => get_post( self::$post_with_image_id )->post_content ] ];
 		// Replace this with some actual testing code.
 		$data = Data\blocks_get_callback( $object );
-		$this->assertEquals( 'core/fake_atts', $data[1]["blockName"] );
-		$this->assertEquals( 'b1', $data[1]["attrs"]["value"] );
-		$this->assertArrayHasKey( 'value', $data[1]["attrs"] );
+		$this->assertEquals( 'core/fake_atts', $data[1]['blockName'] );
+		$this->assertEquals( 'b1', $data[1]['attrs']['value'] );
+		$this->assertArrayHasKey( 'value', $data[1]['attrs'] );
 	}
 
 	/**
@@ -130,13 +130,13 @@ class BlocksTest extends WP_UnitTestCase {
 	 */
 	public function test_image_blocks_attrs() {
 		$object = [ 'content' => [ 'raw' => get_post( self::$post_with_blocks_id )->post_content ] ];
-		$data = Data\blocks_get_callback( $object );
-		$this->assertEquals( 'core/image', $data[0]["blockName"] );
-		$this->assertArrayHasKey( 'src', $data[0]["attrs"] );
-		$this->assertArrayHasKey( 'alt', $data[0]["attrs"] );
-		$this->assertArrayHasKey( 'caption', $data[0]["attrs"] );
-		$this->assertEquals( 'https://cldup.com/YLYhpou2oq.jpg', $data[0]["attrs"]["src"] );
-		$this->assertEquals( 'Test alt', $data[0]["attrs"]["alt"] );
-		$this->assertEquals( 'Give it a try. Press the "really wide" button on the image toolbar.', $data[0]["attrs"]["caption"] );
+		$data   = Data\blocks_get_callback( $object );
+		$this->assertEquals( 'core/image', $data[0]['blockName'] );
+		$this->assertArrayHasKey( 'src', $data[0]['attrs'] );
+		$this->assertArrayHasKey( 'alt', $data[0]['attrs'] );
+		$this->assertArrayHasKey( 'caption', $data[0]['attrs'] );
+		$this->assertEquals( 'https://cldup.com/YLYhpou2oq.jpg', $data[0]['attrs']['src'] );
+		$this->assertEquals( 'Test alt', $data[0]['attrs']['alt'] );
+		$this->assertEquals( 'Give it a try. Press the "really wide" button on the image toolbar.', $data[0]['attrs']['caption'] );
 	}
 }
