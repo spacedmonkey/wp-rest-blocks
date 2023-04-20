02b8338ce33afa8239903dc1a03a81546a32b762
Jonny Harris <jon@spacedmonkey.co.uk>
2023-04-20 21:11:13 +0100

    Bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,11 +5,8 @@
  * @package Wp_Rest_Blocks
  */
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-
-if ( ! $_tests_dir ) {
-	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
-}
+require_once dirname( dirname( __FILE__ ) ) .  '/vendor/yoast/wp-test-utils/src/WPIntegration/bootstrap-functions.php';
+$_tests_dir = Yoast\WPTestUtils\WPIntegration\get_path_to_wp_test_dir();
 
 if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
@@ -19,8 +16,7 @@ if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
-require_once dirname( dirname( __FILE__ ) ) .  '/vendor/yoast/wp-test-utils/src/BrainMonkey/bootstrap.php';
-require_once dirname( dirname( __FILE__ ) ) .  '/vendor/autoload.php';
+
 
 /**
  * Manually load the plugin being tested.
@@ -29,6 +25,8 @@ function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/wp-rest-blocks.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+Yoast\WPTestUtils\WPIntegration\bootstrap_it();
 
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
