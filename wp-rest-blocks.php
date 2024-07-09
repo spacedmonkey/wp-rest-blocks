d94f9d3a255b9d03408da413bc4fa92bd4600124
Guy Cohen <g.cohen@avatrade.com>
2024-07-09 05:49:48 +0000

    fix: prevent code from running in wp-admin

--- wp-rest-blocks.php
+++ wp-rest-blocks.php
@@ -19,6 +19,8 @@ namespace WP_REST_Blocks;
 use WP_REST_Blocks\Posts;
 use WP_REST_Blocks\Widgets;
 
+if ( is_admin() ) return;
+
 if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
 	require_once __DIR__ . '/vendor/autoload.php';
 }
